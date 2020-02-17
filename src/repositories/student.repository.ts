57d9764193b4c0f3d9df6a0898c37cc7a9fb1fdd
Jorge <00jfernandez@gmail.com>
2020-02-16 19:45:20 -0600

    Agregando funcionalidad para manejo de departamentos y creando relacion belongsTo

--- src/repositories/student.repository.ts
+++ src/repositories/student.repository.ts
@@ -1,16 +1,22 @@
-import {DefaultCrudRepository} from '@loopback/repository';
-import {Student, StudentRelations} from '../models';
+import {DefaultCrudRepository, repository, BelongsToAccessor} from '@loopback/repository';
+import {Student, StudentRelations, Department} from '../models';
 import {Pglb4DemoDataSource} from '../datasources';
-import {inject} from '@loopback/core';
+import {inject, Getter} from '@loopback/core';
+import {DepartmentRepository} from './department.repository';
 
 export class StudentRepository extends DefaultCrudRepository<
   Student,
   typeof Student.prototype.id,
   StudentRelations
 > {
+
+  public readonly department: BelongsToAccessor<Department, typeof Student.prototype.id>;
+
   constructor(
-    @inject('datasources.pglb4demo') dataSource: Pglb4DemoDataSource,
+    @inject('datasources.pglb4demo') dataSource: Pglb4DemoDataSource, @repository.getter('DepartmentRepository') protected departmentRepositoryGetter: Getter<DepartmentRepository>,
   ) {
     super(Student, dataSource);
+    this.department = this.createBelongsToAccessorFor('department', departmentRepositoryGetter,);
+    this.registerInclusionResolver('department', this.department.inclusionResolver);
   }
 }
