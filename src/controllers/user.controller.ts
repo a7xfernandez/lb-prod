7f4fdd3bb63bc701009d9c1d38dc0068d7027af4
Jorge <00jfernandez@gmail.com>
2020-02-26 20:38:32 -0600

    Importar lodash para verificacion de email

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,6 +2,8 @@ import { repository } from "@loopback/repository";
 import { UserRepository } from "../repositories";
 import { post, getJsonSchema, requestBody } from "@loopback/rest";
 import { User } from "../models";
+import * as _ from 'lodash';
+import { validateCredentials } from '../services/validator';
 
 // Uncomment these imports to begin using these cool features!
 
@@ -26,6 +28,7 @@ export class UserController {
   })
 
   async signup(@requestBody() userData: User) {
+    validateCredentials(_.pick(userData, ['email', 'password']));
     const savedUser = await this.userRepository.create(userData);
     delete savedUser.password;
     return savedUser;
--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,13 +1,18 @@
-import {DefaultCrudRepository} from '@loopback/repository';
-import {User, UserRelations} from '../models';
-import {Pglb4DemoDataSource} from '../datasources';
-import {inject} from '@loopback/core';
+import { DefaultCrudRepository } from '@loopback/repository';
+import { User, UserRelations } from '../models';
+import { Pglb4DemoDataSource } from '../datasources';
+import { inject } from '@loopback/core';
+
+export type Credentials = {
+  email: string;
+  password: string;
+}
 
 export class UserRepository extends DefaultCrudRepository<
   User,
   typeof User.prototype.id,
   UserRelations
-> {
+  > {
   constructor(
     @inject('datasources.pglb4demo') dataSource: Pglb4DemoDataSource,
   ) {
